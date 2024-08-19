db308e187ed8e6a0e69267ce0aa47a44e453722a
RondinoniJuan <162723034+RondinoniJuan@users.noreply.github.com>
2024-08-19 14:22:43 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,13 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+
+    Minusculas = f"{first_name.lower()} {last_name.lower()}"
+    Titulo = f"{first_name.title()} {last_name.title()}"
+    Mayusculas = f"{first_name.upper()} {last_name.upper()}"
+    Tabulado = f"\t{first_name.lower()} {last_name.lower()}"
+
+    print(Minusculas)
+    print(Titulo)
+    print(Mayusculas)
+    print(Tabulado)
